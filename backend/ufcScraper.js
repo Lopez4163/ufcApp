8df08f7c4401a34677d3b03fc1a22254919a1e5f
Nicholas Lopez <64043616+Lopez4163@users.noreply.github.com>
2023-11-21 21:25:38 -0600

    finished scrape code for fighter, next is page loop

--- backend/ufcScraper.js
+++ backend/ufcScraper.js
@@ -1,37 +1,43 @@
-import puppeteer from "puppeteer"
+import puppeteer from "puppeteer";
+import { createLogger } from "vite";
 
-async function scrapeUFCPage(pageNumber) {
-    const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
+const main = async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(
+    "https://www.ufc.com/athletes/all?filters%5B0%5D=status%3A23&gender=All&search=&page=1",
+    { timeout: 50000 },
+  );
 
-    // Starts
-    await page.goto(`https://www.ufc.com/athletes/all?filters%5B0%5D=status%3A23&gender=All&search=&page=${pageNumber}`);
-
-    // Wait for the content to load (you may need to adjust the selector based on the page structure)
-    await page.waitForSelector('.view-athletes');
-
-    // Extract data
-    const scrapedData = await page.evaluate(() => {
-        const fighterArray = [];
-
-        // Example: Extracting athlete names
-        const athleteElements = document.querySelectorAll('.views-field-title .field-content');
-        athleteElements.forEach((athleteElement) => {
-            const name = athleteElement.textContent.trim();
-            fighterArray.push({ name });
-        });
-
-        // You can add more logic here to extract other data like weight class, record, etc.
-
-        return fighterArray;
+  const fighterScrape = await page.evaluate(() => {
+    const fightersArray = [];
+    const fighters = Array.from(
+      document.querySelectorAll(".c-listing-athlete-flipcard__front"),
+    );
+    fighters.forEach((element) => {
+      imgSrc =
+        element
+          .querySelector(".c-listing-athlete__thumbnail img")
+          ?.getAttribute("src") || "";
+      let name = element
+        .querySelector(".c-listing-athlete__name")
+        .textContent.trim();
+      let nickname = element
+        .querySelector(".c-listing-athlete__nickname")
+        .textContent.trim();
+      let weight_Class = element
+        .querySelector(".c-listing-athlete__title .field__item")
+        .textContent.trim();
+      let record = element
+        .querySelector(".c-listing-athlete__record")
+        .textContent.trim();
+      if (nickname === "") {
+        nickname += "No Nickname";
+      }
+      fightersArray.push(imgSrc, name, nickname, weight_Class, record);
     });
-
-    // Print the scraped data (you might want to save it to a file instead)
-    console.log(scrapedData);
-
-    // Close the browser
-    await browser.close();
-}
-
-// Run the function with a specific page number
-scrapeUFCPage(86);
+    return fightersArray;
+  });
+  console.log(fighterScrape);
+};
+main();
