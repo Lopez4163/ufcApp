fd0d17e4d46442920fc4000792dceceb3060c232
Nicholas Lopez <Lopez4163@gmail.com>
2023-12-25 23:33:36 -0600

    second scraper work

--- backend/ufcScraper2.0.cjs
+++ backend/ufcScraper2.0.cjs
@@ -1,36 +1,74 @@
 const puppeteer = require('puppeteer');
 
-const main = async () => {
-  const browser = await puppeteer.launch({ headless: true });
+async function scrapeFighterData() {
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
-  try {
-    await page.goto(`https://www.roster.watch/`, { timeout: 15000 }); // Reduce timeout to 15 seconds
+  // Navigate to the initial page
+  await page.goto('https://www.ufc.com/athletes/all?filters%5B0%5D=status%3A23');
 
-    await page.waitForSelector('.rt-tbody'); // Wait for the selector to be present
+  // Loop through each fighter
+  const fighterLinks = await page.$$('.c-listing-athlete-flipcard__inner');
+  for (const fighterLink of fighterLinks) {
+    // Hover over the fighter
+    await fighterLink.hover();
+    await fighterLink.hover();
+    await fighterLink.hover();
 
-    const rosterWatchScrape = await page.evaluate(() => {
-      const scraperArray = [];
-      const rosterWatchTable = Array.from(document.querySelectorAll(".rt-tbody"));
-
-      rosterWatchTable.forEach((element) => {
-        const rosterObject = {
-          name: element.querySelector(".rt-tr").textContent.trim()
-          // country: element.querySelector(".rt-td .rt-align-left .rt-valign-center").innerHTML
-
-        };
-        scraperArray.push(rosterObject);
+    // Click on "View Profile"
+    await page.click('.c-listing-athlete-flipcard__action a');
+    console.log('clicked view profile')
+    await page.evaluate(() => {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve();
+        }, 14000);
       });
-
-      return scraperArray;
     });
 
-    console.log(rosterWatchScrape);
-  } catch (error) {
-    console.error('Error during scraping:', error);
-  } finally {
-    await browser.close();
-  }
-};
+    // const hovertest = await page.waitForSelector('.stats-records stats-records--two-column');
+    // await hovertest.hover();
 
-main();
+    // Scrape data from the profile page
+    const fighterData = await page.evaluate(() => {
+      const fighterStatsArray = [];
+        const fighterStats = Array.from(
+      document.querySelectorAll(".l-page")
+        );
+        fighterStats.forEach((element) => {
+
+            const fighterStatsObject = {
+                // imgSrc :
+                // element
+                //     .querySelector(".c-listing-athlete__thumbnail img")
+                //     ?.getAttribute("src") || "",
+                winsByKnockout: element
+                    .querySelector(".c-stat-compare__group ")
+                    .innerHTML
+                // nickname:
+                //     element
+                //         .querySelector(".c-listing-athlete__nickname")
+                //         .textContent.trim() || "No Nickname",
+                // weightClass: element
+                //     .querySelector(".c-listing-athlete__title .field__item")
+                //     .textContent.trim(),
+                // record: element
+                //     .querySelector(".c-listing-athlete__record")
+                //     .textContent.trim(),
+            };
+            fighterStatsArray.push(fighterStatsObject);
+        });
+        return fighterStatsArray;
+    });
+
+    console.log('Fighter Data:', fighterData );
+
+    // Navigate back to the original page
+    await page.goBack();
+  }
+
+  // Close the browser
+  // await browser.close();
+}
+
+scrapeFighterData();
