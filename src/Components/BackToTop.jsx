8d0085a1aa8e9b4b9ceda101b9c4af567626d80f
Nicholas Lopez <64043616+Lopez4163@users.noreply.github.com>
2023-12-30 21:19:58 -0600

    ...

--- src/Components/BackToTop.jsx
+++ src/Components/BackToTop.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./styling/BackToTop.css";
+import "../styling/BacktoTop.css";
 
 const BackToTop = ({ fighterListRef }) => {
   const [backToTop, setBackToTop] = useState(false);
